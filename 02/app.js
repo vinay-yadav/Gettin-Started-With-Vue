da6e645bf462c17604bf8d025cee781238cf4e1c
vinay-yadav <yadav.vinay1508@gmail.com>
2021-09-28 16:14:04 +0530

    v-html & v-bind

--- 02/app.js
+++ 02/app.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Learn Vue!!",
+      courseGoalA: "Learn Vue!!",
+      //   courseGoalB: "<h2>Master Vue & build amazing apps.</h2>",
+      courseGoalB: "Master Vue & build amazing apps.",
       vueLink: "https://youtube.com",
     };
+  },
+  methods: {
+    ouputGoal() {
+      console.log(this);
+      const random = Math.random();
+
+      if (random < 0.5) return this.courseGoalA;
+
+      return this.courseGoalB;
+    },
   },
 });
 
