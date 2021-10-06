91f321e294bce63614d8c615bb4f40c768252849
vinay-fero <vinay.yadav@fero.ai>
2021-10-06 14:31:07 +0530

    component dynamic props

---
+++ 05 - vue-first-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <header>
+      <h1>My friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
