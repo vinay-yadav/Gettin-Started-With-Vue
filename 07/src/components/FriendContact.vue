4673075d575136b5ccc78f55bf4a8da9c46dcde2
vinay-fero <vinay.yadav@fero.ai>
2022-01-01 17:28:32 +0530

    basics of components (props $ emits)

---
+++ 07/src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="showDetails = !showDetails">Show Details</button>
+    <ul v-if="showDetails">
+      <li><strong>Phone:</strong> {{friend.phone}}</li>
+      <li><strong>Email:</strong> {{friend.email}}</li>
+    </ul>
+<!--      <button @click="onDelete(friend.id)">Delete</button>-->
+      <button @click="$emit('delete-contact', friend.id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['friend'],
+    emits: ['delete-contact'],
+  data() {
+    return {
+      showDetails: false,
+    }
+  },
+    methods: {
+        onDelete(id) {
+            this.$emit('delete-contact', id);
+        }
+    }
+}
+</script>
+
